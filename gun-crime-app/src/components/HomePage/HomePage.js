7aad6b591ade094066a0a537088d520c7a2297aa
Jack Gay <jackpowis@gmail.com>
2019-04-23 19:45:51 -0400

    fifth commit: altered some of the aesthetics

--- gun-crime-app/src/components/HomePage/HomePage.js
+++ gun-crime-app/src/components/HomePage/HomePage.js
@@ -61,9 +61,21 @@ class HomePage extends Component {
         <h1 className="is-size-4"><strong>NYC Shooting Incident Data Map (2018)</strong></h1>
         <SubmitFormBorough submitFunc={this.submitFunc} />
         <br />
+        <div className="columns">
+        <div className="column">
         <div className="mapContainer" isAlign="center">
           <Map component={Map} crimeData={crimeData} />
         </div>
+        </div>
+
+        <div className="column">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        </div>
+
+      {/* NEED TO WORK ON DATE FILTER, NEED TO WORK ON BULMA SUBMIT, NEED TO MOVE MAP TO LOCATION OF THE AREA */}
+
+
         <div className="crimeContainer">
           {/* <h1 className="is-size-4">Shooting Incident Data</h1> */}
           <CrimesList crimeData={crimeData} />
