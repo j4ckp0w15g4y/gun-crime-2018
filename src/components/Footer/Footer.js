ada30bf25e4585beb1c3962d474a9e4c29b6f0a3
Jack Gay <jackpowis@gmail.com>
2019-04-26 14:14:58 -0400

    commit to test

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="footer">
+                    <div className="content has-text-centered">
+                        <p> Created in 2019 </p>
+                    </div>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
+
+
